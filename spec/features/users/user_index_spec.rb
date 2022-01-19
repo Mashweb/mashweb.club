ef8578d6226e7df48c0db8bb05883a8c49de430e
Oleg Bavaev <csolg7@gmail.com>
2022-01-19 18:26:33 +1000

    Migrate to FactoryBot

--- spec/features/users/user_index_spec.rb
+++ spec/features/users/user_index_spec.rb
@@ -16,7 +16,7 @@ feature 'User index page', :devise do
   #   When I visit the user index page
   #   Then I see my own email address
   scenario 'user sees own email address' do
-    user = FactoryGirl.create(:user, :admin)
+    user = FactoryBot.create(:user, :admin)
     signin(user.email, user.password)
     visit users_path
     expect(page).to have_content user.email
