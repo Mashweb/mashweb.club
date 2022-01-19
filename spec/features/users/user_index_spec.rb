ed1d5c8f98f3a7b4dc28610a6f6ff0b817ac6df2
Oleg Bavaev <csolg7@gmail.com>
2022-01-19 17:30:25 +1000

    Fix spec/features/users/user_index_spec.rb

--- spec/features/users/user_index_spec.rb
+++ spec/features/users/user_index_spec.rb
@@ -17,7 +17,7 @@ feature 'User index page', :devise do
   #   Then I see my own email address
   scenario 'user sees own email address' do
     user = FactoryGirl.create(:user, :admin)
-    login_as(user, scope: :user)
+    signin(user.email, user.password)
     visit users_path
     expect(page).to have_content user.email
   end
